868ebac5908f8ab477f2c877ed4d48eb7db3f053
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-12 15:03:41 +0300

    change div text to Hello Faderr

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Hi() {
-  return <div> Hello World!</div>;
+  return <div> Hello Faderr</div>;
 }
 
 ReactDOM.render(<Hi />, document.querySelector("#root"));
