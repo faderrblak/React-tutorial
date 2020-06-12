cb77b627494c16491e7e675f81125d3b01c8a8d1
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-12 19:40:01 +0300

    room component setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Room = () => <div className="room">The room is lit</div>
+
+ReactDOM.render(<Room />, document.getElementById("root"));
