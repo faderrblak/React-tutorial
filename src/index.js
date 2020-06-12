e6beb64c6cb93279fe6d8e06d3d0510558ab3a4e
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-12 15:21:29 +0300

    Add new elements h3 and ul

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Hi() {
-  return <div> Hello <strong>Faderr</strong></div>;
+  return <div> 
+    Hello <strong>Faderr</strong>
+    <h3>Favorite Foods:</h3>
+    <ul>
+      <li>Pizza</li>
+      <li>Burgers</li>
+    </ul>
+    
+  </div>;
 }
 
 ReactDOM.render(<Hi />, document.querySelector("#root"));
