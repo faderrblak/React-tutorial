371e8d4807544e4573790028a41170ec660cf856
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-12 15:35:45 +0300

    new css doc and new class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function Hi() {
-  return <div> 
+  return <div className="practice"> 
     Hello <strong>Faderr</strong>
     <h3>Favorite Foods:</h3>
     <ul>
       <li>Pizza</li>
       <li>Burgers</li>
     </ul>
-    
+    {2+5}
   </div>;
 }
 
