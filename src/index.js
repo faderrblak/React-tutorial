861ebd1d0cf3918378ba6ecd299b2a6c244f6803
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-14 18:11:50 +0300

    add useState hook and flip button to change state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-const Room = () => <div className="room">The room is lit</div>
+const Room = () => {
+  const [isLit, setLit] = useState(false);
+
+  return (
+    <div className="room">
+      The room is {isLit ? 'lit' : 'dark'}
+      <br/>
+      <button onClick = {() => setLit(!isLit)}>
+        flip
+      </button>
+    </div>
+  );
+}
 
 ReactDOM.render(<Room />, document.getElementById("root"));
