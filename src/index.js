03c6487567c64f488f5f470aa5c021411da0e732
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-14 23:23:48 +0300

    add temperature state

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css"
 
 const Room = () => {
-  const [isLit, setLit] = useState(false);
+  const [isLit, setLit] = useState(true);
+  const [temp, setTemp] = useState(25)
+  
   const brightness = isLit ? "lit" : "dark";
 
   return (
     <div className={`room ${brightness}`}>
       The room is {isLit ? 'lit' : 'dark'}
+      <br/>
+      The temperature is {temp} Celcius
       <br/>
       <button onClick = {() => setLit(true)}>
         On
