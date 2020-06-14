ba8a216cefdbc332667ceaab5ffb21015d39b779
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-15 00:26:55 +0300

    new index js with Reddit component

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Room = () => {
-  const [isLit, setLit] = useState(true);
-  const [temp, setTemp] = useState(25)
-  
-  const brightness = isLit ? "lit" : "dark";
+const Reddit = () => {
+  const [posts, setPosts] = React.useState([]);
 
   return (
-    <div className={`room ${brightness}`}>
-      The room is {isLit ? 'lit' : 'dark'}
-      <br/>
-      <button onClick = {() => setLit(true)}>
-        On
-      </button>
-      <button onClick = {() => setLit(false)}>
-        Off
-      </button>
-      <br/>
-      The temperature is {temp} Celcius
-      <br/>
-      <button onClick = {() => setTemp(temp + 1)}>
-        +
-      </button>
-      <button onClick={() => setTemp(temp - 1)}>
-        -
-      </button>
+    <div>
+      <h1>/r/react js</h1>
     </div>
   );
 }
 
-ReactDOM.render(<Room />, document.getElementById("root"));
+ReactDOM.render(<Reddit />, document.getElementById("root"));
