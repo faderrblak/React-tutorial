922c82bae4d99a289e2a046bc325e9e7b2b91c8e
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-14 23:05:48 +0300

    add css to change page color on flip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
 const Room = () => {
   const [isLit, setLit] = useState(false);
+  const brightness = isLit ? "lit" : "dark";
 
   return (
-    <div className="room">
+    <div className={`room ${brightness}`}>
       The room is {isLit ? 'lit' : 'dark'}
       <br/>
       <button onClick = {() => setLit(!isLit)}>
