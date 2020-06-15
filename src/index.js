32f02417a96e90a9352d2a38d4cd6d07c7ba7f9c
Faderr Blak <42776617+faderrblak@users.noreply.github.com>
2020-06-15 13:06:03 +0300

    Add useEffect to publish reddit titles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const Reddit = () => {
   const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+    .then(res => {
+      const newPosts = res.data.data.children
+      .map(obj => obj.data);
+      setPosts(newPosts)
+    })
+  }, [])
 
   return (
     <div>
